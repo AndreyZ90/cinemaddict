d972a7b17b8b0020dfd1c1b8e872812bfaf2ebaa
Andrey <zakharov90@gmail.com>
2021-02-27 17:42:35 +0300

    Add mocks (const)

---
+++ src/mocks/const.js
@@ -0,0 +1,173 @@
+export const titles = [
+  `The Shawshank Redemption`,
+  `The Godfather`,
+  `The Godfather: Part II`,
+  `Pulp Fiction`,
+  `The Good, the Bad and the Ugly`,
+  `The Dark Knight`,
+  `12 Angry Men`,
+  `Schindler's List`,
+  `The Lord of the Rings: The Return of the King`,
+  `Fight Club`,
+  `Star Wars: Episode V - The Empire Strikes Back`,
+  `The Lord of the Rings: The Fellowship of the Ring`,
+  `One Flew Over the Cuckoo's Nest`,
+  `Inception`,
+  `Goodfellas`,
+  `Star Wars`,
+  `Seven Samurai`
+];
+
+export const posters = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+
+export const descriptions = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
+export const genres = [
+  `Action`,
+  `Adventure`,
+  `Documentary`,
+  `Drama`,
+  `Erotic`,
+  `Historical`,
+  `Horror`,
+  `Independent`,
+  `Legal`,
+  `Live Action`,
+  `Musical`,
+  `Mystery`,
+  `Romance`,
+  `Satire`,
+  `Spy`,
+  `Superhero`,
+  `Supernatural`,
+  `Thriller`,
+  `War`,
+  `Western`
+];
+
+export const countries = [
+  `Russia`,
+  `USA`,
+  `Brazil`,
+  `Germany`,
+  `Finlad`,
+  `France`,
+  `Spain`,
+  `Estonia`,
+  `Ukraine`,
+  `Italy`
+];
+
+export const actors = [
+  `Catherine Missal`,
+  `Monica Bellucci`,
+  `Michael Doven`,
+  `Jasmine Reate`,
+  `Tom Cruise`,
+  `Scarlett Johansson`,
+  `Anna Raadsveld`,
+  `Jason Statham`,
+  `Emilia Clarke`,
+  `Wentworth Miller`,
+  `Dwayne Johnson`,
+  `Rose Byrne`,
+  `Rachel McAdams`,
+  `Michelle Rodriguez`,
+  `Tom Hanks`,
+  `Jake Gyllenhaal`,
+  `Tom Hardy`,
+  `Chloë Grace Moretz`,
+  `Johnny Depp`,
+  `Arnold Schwarzenegger`,
+  `Sarah Wayne Callies`,
+  `Vincent Cassel`,
+  `Lisa Ulliel`,
+  `Rachel Weisz`,
+];
+
+export const directors = [
+  `Andy Fickman`,
+  `Oz Perkins`,
+  `Cate Shortland`,
+  `Michael Dougherty`,
+  `Francis Ford Coppola`,
+  `Marielle Heller`,
+  `Paul Feig`,
+  `Steven Spielberg`,
+  `James Cameron`,
+  `Roger Kumble`,
+  `Quentin Tarantino`
+];
+
+export const writers = [
+  `L. Frank Baum`,
+  `Frank Joslyn Baum`,
+  `Edgar Allan Woolf`,
+  `Betty Comden`,
+  `Adolph Green`,
+  `Stewart Stern`,
+  `Irvin Shulman`,
+  `Nicholas Ray`,
+  `Sal Mineo`,
+  `Jim Backus`,
+  `Ann Doran`,
+  `Corey Allen`,
+  `William Hopper`,
+  `Rochelle Hudson`,
+  `Dennis Hopper`,
+  `Edward Platt`,
+  `Steffi Sidney`,
+  `Marietta Canty`,
+  `Virginia Brissac`,
+  `Beverly Long`,
+];
+
+export const authors = [
+  `Ann`,
+  `John`,
+  `Angelina`,
+  `Kristian`,
+  `Mohhamed`,
+  `Ivan`,
+  `Zulfiya`,
+  `Katrin`,
+  `Jess`,
+  `Olivia`,
+  `Patrick`
+];
+
+export const comments = [
+  `Or will he prepare the ground for cuts to public spending? `,
+  `Or will he instead signal that the pain of cuts and taxes will have to come, but just not yet?`,
+  `We went big, we went early, but there is more to come and there will be more to come in the budget.`,
+  `I want to level with people about the challenge`,
+  `We all know there are going to be cuts or tax rises`,
+  `No society can ever tax its way to prosperity.`,
+  `More details on how a proposed infrastructure bank will work`,
+  `A fourth round of 3-month grants to self-employed people at up to 80% of their pre-pandemic earnings`,
+  `A new state-backed loan program to help companies recover from the pandemic`,
+  `Yet Sunak sees big risks in doing nothing to address the U.K.’s pandemic-ravaged public finances`
+];
+
+export const emotions = [`smile`, `sleeping`, `puke`, `angry`];
+
