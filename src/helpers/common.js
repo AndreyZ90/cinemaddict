6367b94141e8a539c144e954d3cde54ae538f969
Andrey <zakharov90@gmail.com>
2021-02-27 17:42:56 +0300

    Add helpers (fn)

---
+++ src/helpers/common.js
@@ -0,0 +1,23 @@
+export const getRandomNumber = (min, max) => {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+};
+
+export const getRandomArray = (array) => {
+  let counter = getRandomNumber(1, array.length);
+  const arrayCopy = [...array];
+  const randomArray = [];
+
+
+  while (counter > 0) {
+    const randomIndex = getRandomNumber(0, arrayCopy.length - 1);
+    const [randomElement] = arrayCopy.splice(randomIndex, 1);
+
+    randomArray.push(randomElement);
+
+    counter--;
+  }
+
+  return randomArray;
+};
+
