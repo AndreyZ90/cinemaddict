93efd8bcbac7875656cf8f64067e17cecb18844c
Andrey <zakharov90@gmail.com>
2021-03-01 12:32:37 +0300

    Refactoring Films component

--- src/components/films.js
+++ src/components/films.js
@@ -1,7 +1,34 @@
-const createFilms = () => {
-  return (
-    `<section class="films"></section>`
-  );
-};
+export default class Films {
+  constructor() {
+    this._element = null;
+  }
 
-export default createFilms;
+  getTemplate(...args) {
+    return this._createTemplate(...args);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = this._createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  _createElement(template) {
+    const div = document.createElement(`div`);
+    div.innerHTML = template;
+
+    return div.firstChild;
+  }
+
+  _createTemplate() {
+    return (
+      `<section class="films"></section>`
+    );
+  }
+}
