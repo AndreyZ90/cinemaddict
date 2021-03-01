299e932a89d9ecdef6ebbefb55f8cb140f8a6611
Andrey <zakharov90@gmail.com>
2021-03-01 12:32:56 +0300

    Refactoring Navigation component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,42 @@
-const createNavigation = () => {
-  return (
-    `<nav class="main-navigation">
-      <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-      </div>
-      <a href="#stats" class="main-navigation__additional">Stats</a>
-    </nav>`
-  );
-};
+export default class Navigation {
+  constructor() {
+    this._element = null;
+  }
 
-export default createNavigation;
+  getTemplate(...args) {
+    return this._createTemplate(...args);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = this._createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  _createElement(template) {
+    const div = document.createElement(`div`);
+    div.innerHTML = template;
+
+    return div.firstChild;
+  }
+
+  _createTemplate() {
+    return (
+      `<nav class="main-navigation">
+        <div class="main-navigation__items">
+          <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+          <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+          <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+          <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+        </div>
+        <a href="#stats" class="main-navigation__additional">Stats</a>
+      </nav>`
+    );
+  }
+}
