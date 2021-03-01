d418ef4e087df341c5d9478b8299a7425c663c86
Andrey <zakharov90@gmail.com>
2021-03-01 12:33:34 +0300

    Refactoring SortList component

--- src/components/sort-list.js
+++ src/components/sort-list.js
@@ -1,11 +1,38 @@
-const createSortList = () => {
-  return (
-    `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
-    </ul>`
-  );
-};
+export default class SortList {
+  constructor() {
+    this._element = null;
+  }
 
-export default createSortList;
+  getTemplate(...args) {
+    return this._createTemplate(...args);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = this._createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  _createElement(template) {
+    const div = document.createElement(`div`);
+    div.innerHTML = template;
+
+    return div.firstChild;
+  }
+
+  _createTemplate() {
+    return (
+      `<ul class="sort">
+        <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+        <li><a href="#" class="sort__button">Sort by date</a></li>
+        <li><a href="#" class="sort__button">Sort by rating</a></li>
+      </ul>`
+    );
+  }
+}
