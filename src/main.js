c49b4030db2c0dd3e30f8f635c38108b53d1a722
Andrey <zakharov90@gmail.com>
2021-03-01 12:34:34 +0300

    Refactoring main.js

--- src/main.js
+++ src/main.js
@@ -1,37 +1,34 @@
-import createUserRank from './components/user-rank.js';
-import createNavigation from './components/navigation.js';
-import createSortList from './components/sort-list.js';
-import createFilms from './components/films.js';
-import createFilmsList from './components/films-list.js';
-import createFilmCard from './components/film-card.js';
-import createShowMoreButton from './components/show-more-button.js';
+import UserRank from './components/user-rank.js';
+import Navigation from './components/navigation.js';
+import SortList from './components/sort-list.js';
+import Films from './components/films.js';
+import FilmsList from './components/films-list.js';
+import FilmCard from './components/film-card.js';
+import ShowMoreButton from './components/show-more-button.js';
 
-import {createFilms as createFilmsMock} from './mocks/films.js';
+import {createFilms} from './mocks/films.js';
+import {render} from './helpers/common.js';
 
 const FILMS_COUNT = 16;
 
-const films = createFilmsMock(FILMS_COUNT);
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+const films = createFilms(FILMS_COUNT);
 
 const headerContainer = document.querySelector(`.header`);
 const mainContainer = document.querySelector(`.main`);
 
-render(headerContainer, createUserRank(), `beforeend`);
-render(mainContainer, createNavigation(), `beforeend`);
-render(mainContainer, createSortList(), `beforeend`);
-render(mainContainer, createFilms(), `beforeend`);
+render(headerContainer, new UserRank().getElement(), `beforeend`);
+render(mainContainer, new Navigation().getElement(), `beforeend`);
+render(mainContainer, new SortList().getElement(), `beforeend`);
+render(mainContainer, new Films().getElement(), `beforeend`);
 
 const filmsContainer = document.querySelector(`.films`);
 
-render(filmsContainer, createFilmsList(), `beforeend`);
+render(filmsContainer, new FilmsList().getElement(), `beforeend`);
 
 const filmsListContainer = filmsContainer.querySelector(`.films-list`);
 const filmListCardContainer = filmsListContainer.querySelector(`.films-list__container`);
 
-films.forEach((film) => render(filmListCardContainer, createFilmCard(film), `beforeend`));
+films.forEach((film) => render(filmListCardContainer, new FilmCard(film).getElement(), `beforeend`));
 
-render(filmsListContainer, createShowMoreButton(), `beforeend`);
+render(filmsListContainer, new ShowMoreButton().getElement(), `beforeend`);
 
